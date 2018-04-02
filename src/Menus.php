975a8faa3713e5634f78f48b187f536633997617
Sam Claughan <sam@sc89.me>
2018-04-02 14:01:27 +0100

    PSR2 Formatting.

--- src/Menus.php
+++ src/Menus.php
@@ -8,7 +8,7 @@
  * @license   MIT
  */
 
-declare( strict_types=1 );
+declare(strict_types=1);
 
 namespace Gamajo\ThemeToolkit;
 
@@ -45,45 +45,49 @@ namespace Gamajo\ThemeToolkit;
  *
  * @package Gamajo\ThemeToolkit
  */
-class Menus extends Brick {
+class Menus extends Brick
+{
 
-	const REGISTER = 'register';
-	const UNREGISTER = 'unregister';
+    const REGISTER = 'register';
+    const UNREGISTER = 'unregister';
 
-	/**
-	 * Apply nav menu registrations and unregistrations.
-	 */
-	public function apply() {
-		if ( $this->config->hasKey( self::REGISTER ) ) {
-			$registerConfig = $this->config->getSubConfig( self::REGISTER );
-			$this->register( $registerConfig->getArrayCopy() );
-		}
+    /**
+     * Apply nav menu registrations and unregistrations.
+     */
+    public function apply()
+    {
+        if ($this->config->hasKey(self::REGISTER)) {
+            $registerConfig = $this->config->getSubConfig(self::REGISTER);
+            $this->register($registerConfig->getArrayCopy());
+        }
 
-		if ( $this->config->hasKey( self::UNREGISTER ) ) {
-			$unregisterConfig = $this->config->getSubConfig( self::UNREGISTER );
-			$this->unregister( $unregisterConfig->getArrayCopy() );
-		}
-	}
+        if ($this->config->hasKey(self::UNREGISTER)) {
+            $unregisterConfig = $this->config->getSubConfig(self::UNREGISTER);
+            $this->unregister($unregisterConfig->getArrayCopy());
+        }
+    }
 
-	/**
-	 * Register nav menus.
-	 *
-	 * @param array $menus Arguments for nav menus to be registered.
-	 */
-	public function register( array $menus ) {
-		array_walk( $menus, function ( array $menu ) {
-			\register_nav_menus( $menu );
-		} );
-	}
+    /**
+     * Register nav menus.
+     *
+     * @param array $menus Arguments for nav menus to be registered.
+     */
+    public function register(array $menus)
+    {
+        array_walk($menus, function (array $menu) {
+            \register_nav_menus($menu);
+        });
+    }
 
-	/**
-	 * Unregister nav menus.
-	 *
-	 * @param array $menus Array of nav menu names to unregister.
-	 */
-	public function unregister( array $menus ) {
-		array_walk( $menus, function ( string $menu ) {
-			\unregister_nav_menu( $menu );
-		} );
-	}
+    /**
+     * Unregister nav menus.
+     *
+     * @param array $menus Array of nav menu names to unregister.
+     */
+    public function unregister(array $menus)
+    {
+        array_walk($menus, function (string $menu) {
+            \unregister_nav_menu($menu);
+        });
+    }
 }
